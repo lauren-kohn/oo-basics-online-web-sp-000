2a3d78840f3da62bb02b2b9a1f53c31af3b0157c
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-25 20:48:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   
-  attr_accessor :genre
+  attr_accessor :author, :page_count, :genre
   
   def initialize(title)
     @title = title
@@ -9,23 +9,7 @@ class Book
   def title
     @title
   end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end 
-  
-  def page_count
-    @page_count
-  end
-  
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
